64bbb4ce2486c68948a7210fcc0ff45f277064a2
Carolina Herbster <carolhmj@gmail.com>
2018-08-16 21:56:33 -0300

    Project

---
+++ src/test/kotlin/blog/BlogApplicationTests.kt
@@ -0,0 +1,17 @@
+package blog
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+        println(">> This is the contextLoads test")
+	}
+
+}
